a0f5ddeff3d196da82ab9bc5c844d9ef1d1b265f
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-10-04 20:53:25 +0200

    master - parseRequest + sendResponse

--- src/main/server/WebServer.java
+++ src/main/server/WebServer.java
@@ -1,73 +1,109 @@
 ///A Simple Web Server (WebServer.java)
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
- * Java Copyright 2001 by Jeff Heaton
- *
- * WebServer is a very simple web-server. Any request is responded with a very
- * simple web-page.
- *
- * @author Jeff Heaton
- * @version 1.0
- */
 public class WebServer {
+    static int PORT_NUMBER = 3000;
+    static String HOST = "localhost";
 
     /**
      * WebServer constructor.
      */
     protected void start() {
-        ServerSocket s;
+        ServerSocket serverSocket;
 
-        System.out.println("Webserver starting up on port 80");
+        System.out.println("Webserver starting up on port " + PORT_NUMBER);
         System.out.println("(press ctrl-c to exit)");
         try {
             // create the main server socket
-            s = new ServerSocket(3000);
+            serverSocket = new ServerSocket(PORT_NUMBER);
         } catch (Exception e) {
             System.out.println("Error: " + e);
             return;
         }
 
-        System.out.println("Waiting for connection");
+        System.out.println("Waiting for connection...");
         for (;;) {
             try {
-                // wait for a connection
-                Socket remote = s.accept();
-                // remote is now the connected socket
-                System.out.println("Connection, sending data.");
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        remote.getInputStream()));
-                PrintWriter out = new PrintWriter(remote.getOutputStream());
-
-                // read the data sent. We basically ignore it,
-                // stop reading once a blank line is hit. This
-                // blank line signals the end of the client HTTP
-                // headers.
-                String str = ".";
-                while (str != null && !str.equals(""))
-                    str = in.readLine();
-
-                // Send the response
-                // Send the headers
-                out.println("HTTP/1.0 200 OK");
-                out.println("Content-Type: text/html");
-                out.println("Server: Bot");
-                // this blank line signals the end of the headers
-                out.println("");
-                // Send the HTML page
-                out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
-                out.flush();
-                remote.close();
+                // wait for a connection and create new socket for client
+                Socket remoteClientSocket = serverSocket.accept();
+                handleClient(remoteClientSocket);
             } catch (Exception e) {
                 System.out.println("Error: " + e);
             }
         }
+    }
+
+    private static void handleClient(Socket client) throws IOException {
+        System.out.println("Connection, sending data : " +  client.toString());
+
+        // create reader to read client request from client's socket
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        //PrintWriter out = new PrintWriter(remote.getOutputStream());
+
+        // create request builder to send response
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line + "\r\n");
+        }
+
+        parseRequest(client, requestBuilder);
+        try {
+            sendResponse(client, requestBuilder);
+        } catch (IOException e)
+        {
+            System.err.println("SendResponse: " + e);
+        }
+        try {
+            sendResponse(client, requestBuilder);
+        } catch (IOException e)
+        {
+            System.err.println("SendResponse: " + e);
+        }
+
+        String request = requestBuilder.toString();
+        System.out.println(request);
+    }
+
+    private static void parseRequest(Socket client, StringBuilder requestBuilder) {
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String method = requestLine[0];
+        String path = requestLine[1];
+        String version = requestLine[2];
+        String host = requestsLines[1].split(" ")[1];
+
+        List<String> headers = new ArrayList<>();
+        //headers starts with 3rd line of the request
+        for (int h = 2; h < requestsLines.length; h++) {
+            String header = requestsLines[h];
+            headers.add(header);
+        }
+
+        String accessLog = String.format("Client %s, method %s, path %s, version %s, host %s, headers %s",
+                client.toString(), method, path, version, host, headers.toString());
+        System.out.println(accessLog);
+    }
+
+    private static void sendResponse(Socket client, StringBuilder requestBuilder) throws IOException {
+        OutputStream clientOutput = client.getOutputStream();
+        clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+        clientOutput.write(("ContentType: text/html\r\n").getBytes());
+        clientOutput.write("\r\n".getBytes());
+        clientOutput.write("<b>It works!</b>".getBytes());
+        clientOutput.write("\r\n\r\n".getBytes());
+        clientOutput.flush();
+        client.close();
     }
 
     /**
